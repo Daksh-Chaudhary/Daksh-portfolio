874aaafdac07e127e1b161ffb923f41682704242
Daksh-Chaudhary <dakshpratapsingh.d@gmail.com>
2024-10-14 18:59:50 +0530

    new commit

---
+++ app/page.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/floating-navbar";
+import { FaHome } from "react-icons/fa";
+import Grid from "@/components/Grid";
+import RecentProjects from "@/components/RecentProjects";
+import { navItems } from "@/data";
+import Approach from "@/components/Approach";
+import Footer from "@/components/Footer";
+
+
+export default function Home() {
+  return (
+    <main className="relative bg-black-100 flex justify-center items-center flex-col  mx-auto sm:px-10 px-5 overflow-clip">
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems}/>
+        <Hero />
+        <Grid />
+        <RecentProjects />
+        <Approach />
+        <Footer />
+      </div>
+    </main>    
+  );
+}
